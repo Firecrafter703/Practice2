b215854e830bec015830aeffc43c3d40994e08b5
DK703 <91508871+Firecrafter703@users.noreply.github.com>
2022-05-01 14:24:10 -0700

    Test complete

--- SkillDemoCode.java
+++ SkillDemoCode.java
@@ -1,5 +1,10 @@
 public class SkillDemoCode
 {
 
+public static int subtract(int m, int n)
+    {
+    //m-n
+    return m;
+    }
 
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,12 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SkillDemoTest {
+    
+@Test
+public void subtractionTests()
+{
+    assertEquals(2, SkillDemoCode.subtract(3,1));
+
+}
+}
